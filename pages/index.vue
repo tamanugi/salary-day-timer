8c954c221311c1a711fa19a55b197c5300bd67ff
tamanugi <tamanugi@gmail.com>
2019-04-11 23:30:30 +0900

    🎉 implment

--- pages/index.vue
+++ pages/index.vue
@@ -1,48 +1,106 @@
 <template lang="pug">
-hello-world
+header.container
+  section.content
+    | 給料日
+    input.input-element(v-model="salaryDate" type="text" value="10")
+    | まであと・・・
+
+  section.timer
+    span.count {{ rest.minutes }}
+    span.unit 分
+    span.count {{ rest.seconds }}
+    span.unit 秒
 </template>
 
-<script>
-import Logo from '~/components/Logo.vue'
-import HelloWorld from '~/components/HelloWorld.vue'
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
 
-export default {
-  components: {
-    Logo,
-    HelloWorld
+@Component
+export default class extends Vue {
+  salaryDate: number = 10
+  rest: any = {minutes: 99999, seconds: 99}
+
+  private timer: any
+
+  mounted() {
+    setInterval(() => {
+      const target = new Date(2019, 5, this.salaryDate, 0, 0, 0).getTime() / 1000
+      const now = new Date().getTime() / 1000
+      const r = target - now
+      this.rest.minutes = Math.floor( r / 60 )
+      this.rest.seconds = Math.floor( r % 60 )
+    }, 1000)
   }
 }
 </script>
 
 <style lang="stylus">
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
+
+@font-face {
+  font-family: kfhimaji;
+  src: url("~assets/fonts/KFhimaji.otf");
 }
 
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
+html, body, .container {
+  height: 100%;
 }
 
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
+/* Reset */
+html, body, h1, p, a, div, section {
+  margin: 0;
+  padding: 0;
+  font-size: 100%;
+  font: inherit;
 }
 
-.links {
-  padding-top: 15px;
-}
+/* Special */
+.content
+  position absolute
+  top 40%
+  // margin-left 55px;
+  font-size: 30px;
+  line-height: 23px;
+
+body
+  background-image url("~assets/background.png") 
+  background-position center center
+  /* コンテンツの高さが画像の高さより大きい時、動かないように固定 */
+  background-attachment fixed
+  /* 画像を常に天地左右の中央に配置 */
+  background-position center center
+   
+  /* 画像をタイル状に繰り返し表示しない */
+  background-repeat no-repeat
+   
+  /* 表示するコンテナの大きさに基づいて、背景画像を調整 */
+  background-size contain
+
+  margin auto
+  width 1280px
+
+  font-family kfhimaji
+  color #404040
+
+// .container
+//   margin 0 auto
+//   width 1280px
+
+.timer
+  position absolute
+  top 50%
+
+  font-size 90px
+
+  .unit
+    margin-left 8px
+    margin-right 16px
+    font-size 32px
+
+.input-element
+  border none
+  background-color rgba(0, 0, 0, 0)
+  font-size 32px
+  width 48px
+  margin 0 8px
+  font-family kfhimaji
 </style>
